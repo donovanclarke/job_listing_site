b6507122ec12f9fe53c9e6fab609f8ac895f1831
Donovan Clarke <donovanclarketx@gmail.com>
2015-11-15 05:00:19 -0500

    add admin feature, working on user views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115063937) do
+ActiveRecord::Schema.define(version: 20151115094800) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "job_title"
@@ -31,18 +31,19 @@ ActiveRecord::Schema.define(version: 20151115063937) do
     t.string   "last_name"
     t.string   "user_name"
     t.string   "company_name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
